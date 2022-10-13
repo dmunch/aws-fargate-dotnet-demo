3036fb04158568ecb4c364f482ca0bd3b05c1ced
Daniel Münch <dani.munch@gmail.com>
2022-10-13 15:00:09 +0200

    Add healthchecks to service

--- src/Demo.Api/Program.cs
+++ src/Demo.Api/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
@@ -10,6 +11,8 @@ var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
+
+app.MapHealthChecks("/health");
 
 app.MapGet("/weatherforecast", () =>
 {
